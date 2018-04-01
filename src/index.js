8a7caab69820825004c39477f9d27a1505afb579
jameskraus <jamespkraus@gmail.com>
2018-04-01 15:24:29 -0400

    Start with some data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 
-const App = () => (
-  <div>
-    <h1>Hello World</h1>
-    <p>This is a starter template</p>
-  </div>
-)
+const data = [
+  { x: 0, y: 5 },
+  { x: 2, y: 8 },
+  { x: 4, y: 7 },
+  { x: 5, y: 8 },
+  { x: 6, y: 9 },
+]
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const App = ({ data }) => {
+  return (
+    <svg viewBox="0 0 10 10">
+      {data.map(({ x, y }) => <circle cx={x} cy={y} r="0.25" />)}
+    </svg>
+  )
+}
+
+ReactDOM.render(<App data={data} />, document.getElementById('root'))
