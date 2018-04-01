a83fc9772600dc13944846384dc85f2fe32fedb5
James Kraus <jamespkraus@gmail.com>
2018-04-01 14:12:48 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
+
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a starter template</p>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
